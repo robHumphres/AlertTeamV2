d04fc9c11507314a2763bbf127daf4d0d140f931
Cameron Massey <cameron_massey@eagles.ewu.edu>
2017-04-27 00:37:43 -0700

    I made the CronUpdater easier to understand
    I edited my files and a little bit of Alex's to make the auto updater
    look nicer and easier to understand. Most of it was seperating
    functionalities into their own files.

--- CronUpdater/autoUpdater.js
+++ CronUpdater/autoUpdater.js
@@ -1,8 +1,6 @@
-var soap = require('soap');
-var url = 'http://www.wsdot.wa.gov/traffic/api/HighwayAlerts/HighwayAlerts.svc';
 var CronJob = require('cron').CronJob;
-var alertDb = require('./alertDb.js');
-var alertDbUtils = require("./alertDbUtil.js");
+var trafficFunction = require("./trafficFunction");
+var weatherFunction = require("./weatherFunction");
 
 //this CronJob object is a repeating timer. It performs the function inside it's second
 //parameter at the time interval specified in the first
@@ -13,54 +11,14 @@ var alertDbUtils = require("./alertDbUtil.js");
 //new alerts.
 var job = new CronJob('*/5 * * * * *', function() {
 
-    getAlertsForSpokaneAreaInCallback(function(result){
-      //using functions from alertPrintUtils it makes the
-      //alerts more viewable
+    trafficFunction.trafficAlerts();
+    //alertArray.forEach(addPostFromAlert);
+    weatherFunction.weatherAlerts();
+    console.log("The alerts have been updated!");
 
-      alertArray = result;
-      alertDbUtils.addArrayToDb(alertArray);
-      //alertArray.forEach(addPostFromAlert);
-      alertDbUtils.weatherAlerts();
-      console.log("The alerts have been updated!");
 
-    });
   }, null,//this can be replaced with a function that runs at the end
           //of the above function
   true, /* Start the job right now */
   'America/Los_Angeles' /* Time zone of this job. */
 );
-
-
-
-
-
-//creates a soap client to the WSDOT api then gets alerts
-//from the Spokane area
-////The callback function is where you can use the data
-//from the spokane alerts. The data can only be used
-//in the callback due to the asynchronous funcionality
-//of node js. If you try to return it from the createClient
-//function it will not work (thats why the callback is used)
-var getAlertsForSpokaneAreaInCallback = function (callback) {
-  //creates a soapClient for the url
-  soap.createClient(url, function(err, client){
-    if(err){
-      console.error(err);
-    }
-    if(typeof client != 'undefined' && client){
-      client.GetAlertsForMapArea({AccessCode:'e0b86f9a-d27f-473e-9068-7c15c919df33',MapArea: 'L3SPOWEST'  }, function(err, result, raw, soapHeader){
-        //console.log(result.GetAlertsForMapAreaResult.Alert);
-        if(err)
-        {
-          console.error(err);
-        }
-        if(typeof result != 'undefined' && result){
-          callback(result.GetAlertsForMapAreaResult.Alert);
-        }
-      });
-    }
-    else{
-      console.log("Invalid soap client");
-    }
-  });
-}
---
+++ CronUpdater/db.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+
+var dbURI = 'mongodb://localhost/userAuthentication';
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function() {
+  console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function(err) {
+  console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function() {
+  console.log('Mongoose disconnected');
+});
+
+// called when process is restarted or terminated
+gracefulShutdown = function(msg, callback) {
+  mongoose.connection.close(function() {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+};
+
+// For app termination
+process.on('SIGINT', function() {
+  gracefulShutdown('app termination', function() {
+    process.exit(0);
+  });
+});
+
+require('./models/post');
+require('./models/wAlert');
+require('./models/trafficAlert');
